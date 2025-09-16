dd9c279a2ecfa0ab9498229eccac34c60df4a284
PokeMasterCP <christian.adrn.perez@gmail.com>
2025-09-15 20:25:39 -0500

    Finished project

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from stats import get_num_words
-from stats import get_num_characters
-from stats import get_sorted_list
+from stats import *
+import sys
 
 def get_book_text(path: str) -> str:
     with open(path) as f:
         contents = f.read()
         return contents
-def format_output(word_count: int, char_count: list) -> str:
+
+def format_output(word_count: int, char_count: list, book: str) -> str:
     """Output formatted data based on input list."""
     output = "============ BOOKBOT ============"
-    output += "\nAnalyzing book found at books/frankenstein.txt..."
+    output += f"\nAnalyzing book found at {book}..."
     output += "\n---------- Word Count ----------"
     output += f"\nFound {word_count} total words"
     output += "\n--------- Character Count -------"
@@ -21,16 +21,20 @@ def format_output(word_count: int, char_count: list) -> str:
             line = f"\n{character}: {num}"
             output += line
     return output
-    
 
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    file = sys.argv[1] 
+    book_text = get_book_text(file)
     word_count = get_num_words(book_text)
     unsorted_char_count = get_num_characters(book_text)
     sorted_char_count = get_sorted_list(unsorted_char_count)
     
-    output = format_output(word_count, sorted_char_count)
+    output = format_output(word_count, sorted_char_count, file)
     print(output)
 
 main()
